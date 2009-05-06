3a6c06c797ae511cbb647fdcfe762cb2527156f6
fangjiansoft <fangjiansoft@e3021d40-3a2f-11de-8227-efdb528b9ea3>
2009-05-06 14:19:51 +0000

    需要python2.6(由于使用了json库),可以偷当前已经成熟的朋友,下手前会简单判断一下,"已偷光"或"已偷过"的不会下手.

--- trunk/zeidianji.py
+++ trunk/zeidianji.py
@@ -1,7 +1,7 @@
 # coding=gbk
 ####configuration begin####
-account_name="fangjiansoft@gmail.com"
-account_password="1234567"
+account_name="qffj@hotmail.com"
+account_password="fangkaixin"
 ####configuration end####
 import urllib2
 opener=urllib2.build_opener(urllib2.HTTPCookieProcessor())
@@ -31,17 +31,47 @@ l=line.split(',')
 l1=l[1]
 param=l1[2:-1]  #remove ' at two ends
 print param
+#param='verify=13814301_1062_13814301_1241615280_1f83fcd2117fd3aca6bbe7da595cf4c0&wwwhost=www.kaixin001.com&roomid=8710&roomsnum=2&fuid=0'
 
-#04 GET /!house/!garden/getconf.php?
+#03.2 split parameter
+plist=param.split('&')
+pdict={}
+for p in plist:
+	pair=p.split('=')
+	pdict[pair[0]]=pair[1]
+
+#getconf function
+#http://www.kaixin001.com/!house/!garden/getconf.php?verify=13814301_1062_13814301_1241613391_54fc24f1c895e2e23fd07e9ad4a4c734&fuid=1230242&r=0.06616138247773051
+def getConf(uid):
+    print param
+    url="/!house/!garden/getconf.php?"
+    url+=("verify=%s" % pdict['verify'])
+    url+=("&fuid=%s" % uid)
+    import random
+    r=random.random()
+    url+=("&r=%s" % r)
+    req = urllib2.Request(url=host+url)
+    f = urllib2.urlopen(req)
+    print "GET "+url
+    print f.msg
+    document=f.read()
+    #print f.read()
+    return document
+
+
+#04 get host's conf
+#GET /!house/!garden/getconf.php?verify=13814301_1062_13814301_1241617242_a02655ab7ab200ed717c3c307b22c30a&fuid=0&r=0.034257106482982635 HTTP/1.1
 url="/!house/!garden/getconf.php?"+param
 req = urllib2.Request(url=host+url)
 f = urllib2.urlopen(req)
 print "GET "+url
 print f.msg
+document=f.read()
 #print f.read().decode('utf-8').encode('gbk')
 
+
+
 #parse host's conf
-document=f.read()
 import xml.dom.minidom
 def getTagText(root, tag):
     node = root.getElementsByTagName(tag)
@@ -56,40 +86,73 @@ def getTagText(root, tag):
     return rc.encode('gbk')
 
 
-dom = xml.dom.minidom.parseString(document)
-ac=dom.getElementsByTagName('account')[0]
-name=getTagText(ac,'name')
-print "进入主人%s的花园..." % name
+
 
 def isMature(info):
+    if(info.find("已偷")>0):
+        return False
+    if(info.find("不能偷")>0):
+        return False
     if(info.find("剩余")>0):
         return True
     else:
         return False
-    
 
-farms=dom.getElementsByTagName('item')
-for farm in farms:
-    status = getTagText(farm,'status')
-    cropsid = getTagText(farm,'cropsid')
-    farmnum=getTagText(farm,'farmnum')
-    #print farmnum
-    if(1==int(status) and int(cropsid)>0):
-        fname=getTagText(farm,'name')
-        crops=getTagText(farm,'crops')
-        print fname,crops
-        #havest
-        url="/!house/!garden/havest.php?"+param
-        import random
-        r=random.random()
-        url+=("&r=%s" % r)
-        url+="&seedid=0"
-        url+=("&farmnum=%s" % farmnum)
-        if (isMature(crops)):
-            print "试图收一下..."
-            req = urllib2.Request(url=host+url)
-            f = urllib2.urlopen(req)
-            #print "GET "+url
-            #print f.msg
-            print f.read().decode('utf-8').encode('gbk')
+def havest(uid,document):
+    dom = xml.dom.minidom.parseString(document)
+    ac=dom.getElementsByTagName('account')[0]
+    name=getTagText(ac,'name')
+    print "进入%s的花园..." % name
+    farms=dom.getElementsByTagName('item')
+    for farm in farms:
+        status = getTagText(farm,'status')
+        cropsid = getTagText(farm,'cropsid')
+        farmnum=getTagText(farm,'farmnum')
+        #print farmnum
+        if(1==int(status) and int(cropsid)>0):
+            fname=getTagText(farm,'name')
+            crops=getTagText(farm,'crops')
+            print fname,crops
+            #havest
+            url="/!house/!garden/havest.php?"
+            url+=("verify=%s" % pdict['verify'])
+            url+=("&fuid=%s" % uid)
+            import random
+            r=random.random()
+            url+="&seedid=0"
+            url+=("&farmnum=%s" % farmnum)
+            if (isMature(crops)):
+                print "试图收一下..."
+                req = urllib2.Request(url=host+url)
+                f = urllib2.urlopen(req)
+                #print "GET "+url
+                #print f.msg
+                print f.read().decode('utf-8').encode('gbk')
+
+#收自家园子的菜
+havest(0,document)
+
+#查看谁家的菜熟了
+#GET /!house/!garden/getfriendmature.php?verify=13814301_1062_13814301_1241613391_54fc24f1c895e2e23fd07e9ad4a4c734&r=0.5315156443975866 HTTP/1.1
+url="/!house/!garden/getfriendmature.php?"+param
+import random
+r=random.random()
+url+=("&r=%s" % r)
+req = urllib2.Request(url=host+url)
+f = urllib2.urlopen(req)
+#print f.read().decode('utf-8').encode('gbk')
+#print f.read()
+
+import json
+m=f.read()
+j=json.loads(m)
+friends=j['friend']
+for friend in friends:
+    uid=friend['uid']
+    realname=friend['realname']
+    print uid,realname
+    document=getConf(uid)
+    havest(uid,document)
+
+
 
